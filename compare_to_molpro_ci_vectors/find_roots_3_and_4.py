7ea6ae5518a4549534ddebaa9ef85429e81fbc21
Judith <judith.leson@stud.uni-due.de>
2026-03-10 18:01:07 +0100

    find_roots_3_and_4.py

---
+++ compare_to_molpro_ci_vectors/find_roots_3_and_4.py
@@ -0,0 +1,32 @@
+import re
+
+from src.building_blocks.get_dimer_states_from_monomer_states import get_dimer_states_from_monomer_states
+from src.symmetries.CI_ORDERING import CI_ORDERING
+from src.symmetries.Molecule import Molecule
+
+
+
+dimer_states = get_dimer_states_from_monomer_states(molecule=Molecule.C6H6, ordering=CI_ORDERING.molpro)
+
+
+
+
+for d in dimer_states:
+    if d.monomer_state_1.label != d.monomer_state_2.label:
+        continue
+    if not "e" in d.monomer_state_1.label:
+        continue
+
+    d.get_product_terms()
+    d.get_determinants()
+    d.sum_up_determinants()
+
+
+    ci_vectors = [i.latex_ci_equation(short_version=True) for i in d.summed_up_list_of_determinants_ci]
+    simplified_ci_vectors = [
+            ('+' if '+' in i else '-') + re.search(r'\\left\|(.*?)\\right\|', i).group(1)
+            for i in ci_vectors
+    ]
+    print(d.get_label(), simplified_ci_vectors)
+
+
